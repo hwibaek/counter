1c3c03f5a755f1a2d6f76adc2109644e9f83e47c
hwibaek <hwibaek2827@gmail.com>
2023-09-20 10:51:18 +0900

    i made it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React, {useState} from "react"; 
+export const App = () => {
+  let [count, setter] = useState(0);
+  const Change = (value) => count = setter(count + value);
+  return(
+      <div>
+        <h1>count : {count}</h1>
+        <button onClick={() => {Change(1)}}>+</button>
+        <button onClick={() => {Change(-1)}}>-</button>
+      </div>
   );
 }
-
-export default App;
